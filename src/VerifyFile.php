8644805874a957c619c36c76bcc4c1b2ba99f104
Ben Batschelet <ben@codeup.com>
2015-05-26 11:46:22 -0500

    Documentation for VerifyFile and functions
    Closes #11

--- src/VerifyFile.php
+++ src/VerifyFile.php
@@ -2,43 +2,96 @@
 
 use BBat\Verify\Assert as a;
 
+/**
+ * Verify Files
+ *
+ * Collection of assertions specific to filesystem objects.
+ *
+ * @package BBat\Verify
+ */
 class VerifyFile extends VerifyBase
 {
+    /**
+     * Assert contents of SUT are equal to another given file's contents
+     *
+     * @param string $expected Name of file SUT is expected to be equal to
+     * @return void
+     */
     public function equals($expected)
     {
         a::assertFileEquals($expected, $this->actual, $this->description);
     }
 
+    /**
+     * Assert contents of SUT are not the same as another given file
+     *
+     * @param string $expected Name of file SUT is expected to differ from
+     * @return void
+     */
     public function doesNotEqual($expected)
     {
         a::assertFileNotEquals($expected, $this->actual, $this->description);
     }
 
+    /**
+     * Assert SUT exists in the filesystem
+     *
+     * @return void
+     */
     public function exists()
     {
         a::assertFileExists($this->actual, $this->description);
     }
 
+    /**
+     * Assert SUT does not exist in the filesystem
+     *
+     * @return void
+     */
     public function doesNotExist()
     {
         a::assertFileNotExists($this->actual, $this->description);
     }
 
+    /**
+     * Assert SUT's contents as JSON value are equal to the JSON contents of a given file
+     *
+     * @param string $file JSON file SUT is expected to be the same as
+     * @return void
+     */
     public function equalsJsonFile($file)
     {
         a::assertJsonFileEqualsJsonFile($file, $this->actual, $this->description);
     }
 
+    /**
+     * Assert SUT's contents as a JSON value are not the same as the JSON contents of a given file
+     *
+     * @param string $file JSON file SUT is expected to differ from
+     * @return void
+     */
     public function doesNotEqualJsonFile($file)
     {
         a::assertJsonFileNotEqualsJsonFile($file, $this->actual, $this->description);
     }
 
+    /**
+     * Assert SUT's contents as XML are the same as the XML in a given file
+     *
+     * @param string $file XML file SUT is expected to be the same as
+     * @return void
+     */
     public function equalsXmlFile($file)
     {
         a::assertXmlFileEqualsXmlFile($file, $this->actual, $this->description);
     }
 
+    /**
+     * Assert SUT's contents as XML are not the same as the XML in a given file
+     *
+     * @param string $file XML file SUT is expected to differ from
+     * @return void
+     */
     public function doesNotEqualXmlFile($file)
     {
         a::assertXmlFileNotEqualsXmlFile($file, $this->actual, $this->description);
--- src/function.php
+++ src/function.php
@@ -4,13 +4,20 @@ use BBat\Verify\Verify;
 use BBat\Verify\VerifyFile;
 
 if (!function_exists('verify')) {
-
     /**
-     * @param $description
-     * @param null $actual
-     * @return \BBat\Verify
+     * Interface into Verify library and main set of assertions
+     *
+     * @param string|mixed $description When called with a single parameter, will be used as the
+     *                                  Subject Under Test (SUT). When called with two parameters,
+     *                                  will be used as a description to display if the assertion fails.
+     * @param mixed        $actual      Optional value for SUT when called with two arguments
+     *
+     * @throws \BadMethodCallException When called with 0 arguments, or more than two arguments
+     *
+     * @return void
      */
-    function verify() {
+    function verify()
+    {
         switch(func_num_args()) {
             case 1:
                 return new Verify(func_get_arg(0));
@@ -21,44 +28,83 @@ if (!function_exists('verify')) {
         }
     }
 
-    function verify_that($truth) {
+    /**
+     * Assert a value is not empty or "true"
+     *
+     * @param mixed $truth SUT expected to be not empty
+     * @return void
+     */
+    function verify_that($truth)
+    {
         verify($truth)->isNotEmpty();
     }
 
-    function verify_not($fallacy) {
+    /**
+     * Assert a value is empty or "false"
+     *
+     * @param mixed $fallacy SUT expected to be empty
+     * @return void
+     */
+    function verify_not($fallacy)
+    {
         verify($fallacy)->isEmpty();
     }
 }
 
 if (!function_exists('expect')) {
+    /**
+     * Alternate function for verify()
+     *
+     * @see verify()
+     *
+     * @return void
+     */
+    function expect()
+    {
+        return call_user_func_array('verify', func_get_args());
+    }
 
     /**
-     * @param $description
-     * @param null $actual
-     * @return \BBat\Verify
+     * Alternate function for verify_that()
+     *
+     * @see verify_that()
+     *
+     * @return void
      */
-    function expect() {
-        return call_user_func_array('verify', func_get_args());
-     }
-
-    function expect_that($truth) {
+    function expect_that($truth)
+    {
         expect($truth)->isNotEmpty();
     }
 
-    function expect_not($fallacy) {
+    /**
+     * ALternate function for verify_not()
+     *
+     * @see verify_not()
+     *
+     * @return void
+     */
+    function expect_not($fallacy)
+    {
         expect($fallacy)->isEmpty();
     }
 
 }
 
 if (!function_exists('verify_file')) {
-
     /**
-     * @param $description
-     * @param null $actual
-     * @return \BBat\Verify
+     * Interface into Verify with assertions specific to file system objects
+     *
+     * @param string $description When called with a single parameter, will be used as the Subject
+     *                            Under Test (SUT) file name. When called with two parameters,
+     *                            will be used as a description to display if the assertion fails.
+     * @param string $actual      Optional file name for SUT when called with two arguments
+     *
+     * @throws \BadMethodCallException When called with 0 arguments, or more than two arguments
+     *
+     * @return void
      */
-    function verify_file() {
+    function verify_file()
+    {
         switch(func_num_args()) {
             case 1:
                 return new VerifyFile(func_get_arg(0));
@@ -72,11 +118,14 @@ if (!function_exists('verify_file')) {
 
 if (!function_exists('expect_file')) {
     /**
-     * @param $description
-     * @param null $actual
-     * @return \BBat\Verify
+     * Alternate function for verify_file()
+     *
+     * @see verify_file()
+     *
+     * @return void
      */
-    function expect_file() {
+    function expect_file()
+    {
         return call_user_func_array('verify_file', func_get_args());
     }
 }
