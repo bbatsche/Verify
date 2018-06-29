677ffcbd1edeef354ab4e30959fd6f47b24622e3
Ben Batschelet <bartleby84@gmail.com>
2018-06-29 15:38:06 -0500

    Delete legacy docs Issue #40

--- docs/elementlist.js
+++
@@ -1,3 +0,0 @@
-
-var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","BeBat\\Verify\\Assert"],["c","BeBat\\Verify\\Verify"],["c","BeBat\\Verify\\VerifyBase"],["c","BeBat\\Verify\\VerifyFile"],["f","expect()"],["f","expect_file()"],["f","expect_not()"],["f","expect_that()"],["f","verify()"],["f","verify_file()"],["f","verify_not()"],["f","verify_that()"]];
